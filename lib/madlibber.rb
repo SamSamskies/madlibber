6fc299922e173f21d58f68cdb74ccdf9e55709b5
Steven Miyakawa SAM <samprofessional@gmail.com>
2014-01-22 17:43:45 -0800

    First version

--- lib/madlibber.rb
+++ lib/madlibber.rb
@@ -1,5 +1,53 @@
 require "madlibber/version"
+require "engtagger"
 
-module Madlibber
-  # Your code goes here...
+module MadLibber
+
+  extend self
+
+  OPTIONS = {
+    num_of_blanks: 10
+  }
+
+  TAGS = {
+    "NN"  => "<noun>",
+    "NNS" => "<plural_noun>",
+    "NNP" => "<proper_noun>",
+    "VB"  => "<verb>",
+    "VBD" => "<verb_past_tense>",
+    "JJ"  => "<adjective>",
+    "RB"  => "<adverb>",
+    "UH"  => "<interjection>"
+  }
+
+  def libberfy text, options = OPTIONS
+    defaults = OPTIONS
+    defaults.merge! options
+    tgr = EngTagger.new
+    tag_word_pairs = tgr.get_readable(text).split.map { |tagged_word| create_tag_word_pairs tagged_word }
+    fillable_indices = find_fillable_indices(tag_word_pairs).shuffle.take defaults[:num_of_blanks]
+
+    tag_word_pairs.map.with_index do |tw_pair, index|
+      (fillable_indices.include? index) ? TAGS[ tw_pair[:tag] ] : tw_pair[:word]
+    end.join(" ")
+  end
+
+  def create_tag_word_pairs word
+    index = ( word =~ /\/\S*$/ )
+    tag = word[(index + 1)..-1]
+    word = word[0..(index - 1)]
+    { word: word, tag: tag }
+  end
+
+  def find_fillable_indices tag_word_pairs
+    fillable_indices = tag_word_pairs.map.with_index do |tw_pair, index|
+      fillable_tag = TAGS[ tw_pair[:tag] ]
+      fillable_tag ? index : nil
+    end.compact!
+
+    fillable_indices ? fillable_indices : []
+  end
+
 end
+
+
--- madlibber.gemspec
+++ madlibber.gemspec
@@ -6,11 +6,11 @@ require 'madlibber/version'
 Gem::Specification.new do |spec|
   spec.name          = "madlibber"
   spec.version       = Madlibber::VERSION
-  spec.authors       = ["Steven Miyakawa SAM"]
+  spec.authors       = ["Sam Samskies"]
   spec.email         = ["samprofessional@gmail.com"]
-  spec.description   = %q{TODO: Write a gem description}
-  spec.summary       = %q{TODO: Write a gem summary}
-  spec.homepage      = ""
+  spec.description   = %q{Turns any chunk of text into a Mad Lib.}
+  spec.summary       = %q{Turns any chunk of text into a Mad Lib}
+  spec.homepage      = "https://github.com/SamSamskies/madlibber"
   spec.license       = "MIT"
 
   spec.files         = `git ls-files`.split($/)
@@ -20,4 +20,6 @@ Gem::Specification.new do |spec|
 
   spec.add_development_dependency "bundler", "~> 1.3"
   spec.add_development_dependency "rake"
+  spec.add_development_dependency "pry"
+  spec.add_dependency "engtagger"
 end
